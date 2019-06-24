f2519e6e8891dc5c819de2659736bbc753059aff
mrivar <martin7557@gmail.com>
2019-06-24 15:11:40 +0200

    Reorganize results. Add analytics.

--- results/plot.py
+++ results/plot.py
@@ -5,6 +5,99 @@ import numpy as np
 
 from plot_utils import *
 
+
+############################
+# iPython
+def ipypthon_plot_log(log_files, prefix, suffix='_valid.txt', col='acc', subplot=None, figure=None,
+                      labels=None, colors = None, linestyles='-',
+                      legend_formatting=None, **kwargs):
+    logs_names = [f for f in log_files if f.endswith(suffix) and f.startswith(prefix)]
+    logs_names.sort()
+    logs = [load_csv(f) for f in logs_names]
+    if figure is not None:
+        ax = figure.add_subplot(subplot or '111', **kwargs)
+        ret = plot_against(logs, col, labels or logs_names, linestyles, colors, ax)
+        ax.grid(color="#f1f1f4")
+        if legend_formatting: ax.legend(loc=4, framealpha=0)
+        else: ax.legend()
+    else:
+        ret = plot_against(logs, col, labels or logs_names, linestyles, colors)
+        plt.grid(color="#f1f1f4")
+        if legend_formatting: plt.legend(loc=4, framealpha=0)
+        else: plt.legend()
+        if subplot is not None: plt.subplot(subplot, **kwargs)
+    return ret
+
+
+
+def ipypthon_plot_cols(log_files, prefix, suffix='_val.log', cols=['acc', 'acc_mean', 'acc_vote'], labels=['regular', 'averaging', 'voting'], subplot=None, figure=None, **kwargs):
+    logs_names = [f for f in log_files if f.endswith(suffix) and f.startswith(prefix) and 'BBB' in f]
+    assert len(logs_names) == 1, '%s'%(str(logs_names))
+    f = logs_names[0]
+    logs = load_csv(f)
+    if subplot is None:
+        plot_cols_against(logs, cols, labels)
+        plt.legend()
+    else:
+        if figure is None:
+            plot_cols_against(logs, cols, labels)
+            plt.legend()
+            plt.subplot(subplot, **kwargs)
+        else:
+            ax = figure.add_subplot(subplot, **kwargs)
+            plot_cols_against(logs, cols, labels, ax)
+            ax.legend()
+
+            
+def ipypthon_plot_sd(log_files, prefix, suffix='_uncertainty.log', what_layer="conv", what_sd='random', subplot=None, figure=None, marker_epoch=None, **kwargs):
+    logs_names = [f for f in log_files if f.endswith(suffix) and f.startswith(prefix) and 'BBB' in f]
+    assert len(logs_names) == 1, '%s'%(str(logs_names))
+    assert what_layer in ['conv', 'fc'] and what_sd in ['random', 'total']
+    f = logs_names[0]
+    log = load_csv(f)
+    sds = get_sd_from_alpha(log[what_sd + '_param_log_alpha'], log[what_sd + '_param_mean'])
+    if what_layer == "fc": sds = get_sd_from_rho(log[what_sd + '_param_rho'])
+    if subplot is None:
+        plt.plot(log['epoch'], sds)
+        plt.grid(color="#f1f1f4")
+    else:
+        if figure is None:
+            plt.plot(log['epoch'], sds)
+            plt.grid(color="#f1f1f4")
+            plt.subplot(subplot, **kwargs)
+        else:
+            ax = figure.add_subplot(subplot, **kwargs)
+            plt.plot(log['epoch'], sds)
+            if marker_epoch: plt.plot(marker_epoch, sds[marker_epoch], marker="*", markersize=8.5)
+            plt.grid(color="#f1f1f4")
+            #ax.legend()
+
+
+def ipypthon_plot_unc(log_files, prefix, suffix='_uncertainty.log', subplot=None, figure=None, what_unc='epistemic', marker_epoch=None, **kwargs):
+    logs_names = [f for f in log_files if f.endswith(suffix) and f.startswith(prefix) and 'BBB' in f]
+    assert len(logs_names) == 1, '%s'%(str(logs_names))
+    assert what_unc in ['epistemic', 'aleatoric']
+    f = logs_names[0]
+    log = load_csv(f)
+    if subplot is None:
+        plt.plot(log['epoch'], log[what_unc])
+        plt.grid(color="#f1f1f4")
+    else:
+        if figure is None:
+            plt.plot(log['epoch'], log[what_unc])
+            plt.grid(color="#f1f1f4")
+            plt.subplot(subplot, **kwargs)
+        else:
+            ax = figure.add_subplot(subplot, **kwargs)
+            plt.plot(log['epoch'], log[what_unc])
+            if marker_epoch: plt.plot(marker_epoch, log[what_unc][marker_epoch], marker="*", markersize=8.5)
+            plt.grid(color="#f1f1f4")
+            #ax.legend()
+
+
+
+############################
+# Plots
 def plot_train_vs_validation(dataset, model, date, counter):
     sets = ['train', 'val']
     csv = [load_csv(complete_name(dataset, model, date, counter, s))
@@ -79,28 +172,60 @@ def plot_total_bayes_vs_freq(dataset, model, sma=1):
     plt.show()
 
 
-def plot_alpha(mean, log_alpha):
-    alphas = np.exp(log_alpha)
-    plt.plot(range(1, len(mean)+1), alphas)
+def plot_alpha(epochs, log_alpha):
+    plot_n_show(epochs, np.exp(log_alpha))
+
+
+def plot_rho(epochs, rho):
+    plot_n_show(epochs, rho)
+
+
+def plot_std_alpha(mu, log_alpha):
+    stds = np.sqrt(pow(mu, 2) * np.exp(log_alpha))
+    plt.plot(range(1, len(mu)+1), stds)
     plt.show()
 
+def plot_std_rho(epochs, rho):
+    plot_n_show(epochs, np.log1p(np.exp(rho)))
 
-def plot_standard_dev(mean, log_alpha):
-    stds = np.sqrt(pow(mean, 2) * np.exp(log_alpha))
-    plt.plot(range(1, len(mean)+1), stds)
-    plt.show()
-
-
-def plot_weights(mean, log_alpha, epochs=[0,19,49,99,499],
+def plot_conv_weights(mu, log_alpha, xlim=0.0002, epochs=[0,19,49,99,499],
     colors=["#A8A7A7", "#CC527A", "#E8175D", "#474747", "#363636"]):
-    stds = pow(mean, 2) * np.exp(log_alpha)
-    means = np.asarray(mean[epochs])
+    stds = pow(mu, 2) * np.exp(log_alpha)
+    mus = np.asarray(mu[epochs])
     stds  = np.asarray(stds[epochs])
 
     for i, e in enumerate(epochs):
-        #distribution = np.random.normal(loc=means[i], scale=stds[i], size=500000)
+        # distribution = np.random.normal(loc=mus[i], scale=stds[i], size=500000)
         distribution = np.random.normal(loc=0, scale=stds[i], size=50000)
         sns.distplot(distribution, hist=False, label="%s"%str(e+1), color=colors[i])
 
-    plt.xlim(-0.0002, 0.0002)
+    if xlim: plt.xlim(-xlim, xlim)
+    plt.show()
+
+def plot_conv_weights_with_mu(mu, log_alpha, epochs=[0,19,49,99,499],
+    colors=["#A8A7A7", "#CC527A", "#E8175D", "#474747", "#363636"]):
+    stds = pow(mu, 2) * np.exp(log_alpha)
+    mus = np.asarray(mu[epochs])
+    stds  = np.asarray(stds[epochs])
+
+    for i, e in enumerate(epochs):
+        distribution = np.random.normal(loc=mus[i], scale=stds[i], size=500000)
+        # distribution = np.random.normal(loc=0, scale=stds[i], size=50000)
+        sns.distplot(distribution, hist=False, label="%s"%str(e+1), color=colors[i])
+
+    plt.show()
+
+
+def plot_fc_weights(mu, rho, xlim=0.0002, epochs=[0,19,49,99,499],
+    colors=["#A8A7A7", "#CC527A", "#E8175D", "#474747", "#363636"]):
+    stds = np.log1p(np.exp(rho))
+    mus = np.asarray(mu[epochs])
+    stds  = np.asarray(stds[epochs])
+
+    for i, e in enumerate(epochs):
+        # distribution = np.random.normal(loc=mus[i], scale=stds[i], size=500000)
+        distribution = np.random.normal(loc=0, scale=stds[i], size=50000)
+        sns.distplot(distribution, hist=False, label="%s"%str(e+1), color=colors[i])
+
+    if xlim: plt.xlim(-xlim, xlim)
     plt.show()
--- results/plot_utils.py
+++ results/plot_utils.py
@@ -1,5 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+
+blue = '#1f77b4'
+orange = '#ff7f0e'
+gray = '#7f7f7f'
 
 def load_csv(name):
     with open(name, 'r') as f:
@@ -18,8 +23,61 @@ def apply_sma(original, sma=1):
     return ders
 
 
-def plot_against(csv, column, label):
+def get_sd_from_alpha(alpha, mu):
+    return np.sqrt(np.exp(alpha) * np.power(mu, 2))
+
+
+def get_sd_from_rho(rho):
+    return np.log(1 + np.exp(rho))
+
+
+def plot_against(csv, column, label, linestyle='-', colors=None, ax=None):
     # @param csv: Array of pandas loaded csv
-    # @param column: Column to plot (e.g. 'acc' or 'loss')
-    for val, lab in zip(csv, label):
-        plt.plot(val.epoch, val[column], label=lab)
+    # @param column: Column to plot (e.g. 'acc' or 'loss'). Can be a tuple or array. If first value not found, try next.
+    if ax is None: ax=plt
+    if type(linestyle) == str: linestyle = [linestyle]*len(csv)
+    if type(column) == str: column=[column]
+    if colors is None: colors = [None]*len(csv)
+    for val, lab, ls, cl in zip(csv, label, linestyle, colors):
+        for col in column:
+            try:
+                ax.plot(val.epoch, val[col], linestyle=ls, color=cl, label=lab)
+                break
+            except:
+                continue
+    return ax
+
+def plot_cols_against(csv, columns, label, ax=None, linestyle='-'):
+    # @param csv: 1 pandas loaded csv
+    # @param column: Array of columns to plot (e.g. ['acc', 'acc_mean'] or 'loss')
+    if ax is None: ax=plt
+    for col, lab in zip(columns, label):
+        ax.plot(csv.epoch, csv[col], linestyle, label=lab)
+
+
+def plot_n_show(y, x):
+    plt.plot(y, x)
+    plt.show()
+
+
+
+def get_max_valid_value_and_train(log_files, bayesian=True, contains=(''), xlim=10000):
+    assert type(contains) == list or type(contains) == tuple
+    if bayesian:
+        valid_model = [f for f in log_files if 'BBB' in f and all(ss in f for ss in contains)][0]
+    else:
+        valid_model = [f for f in log_files if 'BBB' not in f and all(ss in f for ss in contains)][0]
+    valid_model_csv = load_csv(valid_model)['acc_mean'][:xlim]
+    max_val = max(valid_model_csv)
+    max_val_index = np.argmax(valid_model_csv)
+    print("Model: %35s \tMax val: %f in epoch %d"%(valid_model, max_val, max_val_index))
+
+    train_model = valid_model.replace('val', 'train')
+    train_model_csv = load_csv(train_model)['acc'][:xlim]
+    # max_val = max(train_model_csv)
+    # max_val_index = np.argmax(train_model_csv)
+    max_val = train_model_csv[max_val_index]
+    print("\t\t\t\t\t\tMax val: %f in epoch %d"%(max_val, max_val_index))
+
+    
+    
